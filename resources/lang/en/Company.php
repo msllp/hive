e1b7e56bf2464138c2c382f2422014d974aedaac
msllp <48418814+msllp@users.noreply.github.com>
2019-12-13 19:26:37 +0530

    lan

---
+++ resources/lang/en/Company.php
@@ -0,0 +1,25 @@
+<?php
+return
+    [
+        'id'=>'CompanyID',
+        'name' =>'Company Name',
+        'shortname'=>'Company Short Code',
+        'city'=>'City/Town',
+        'ad1'=>'Address Line 1',
+        'ad2'=>'Address Line 2',
+        'ad3'=>'Address Line 3',
+        'pincode'=>'Pincode',
+        'state'=>'State',
+        'pincode'=>'Pincode',
+        'gst'=>'GST No.',
+        'pan'=>'PAN/TAN No.',
+        'cin'=>'CIN No.',
+        'type'=>'Type of Company',
+        'con'=>'Contact No.',
+        'email'=>'Email',
+        'role'=>'Role',
+        'hasbranch'=>'Has a Branches ?'
+
+
+
+    ];
